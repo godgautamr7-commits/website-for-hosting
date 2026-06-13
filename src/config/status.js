33623d45eae5b7567a4db30a8e6ca9f5c47f8130
diablo server <godgautamr7@gmail.com>
2026-06-13 11:52:54 +0530

    Update status.js

--- src/config/status.js
+++ src/config/status.js
@@ -1,6 +1,6 @@
 export const statusServices = [
-  { name: "Game Panel", url: "https://panel.haneknodes.dpdns.org", uptime: "99.9%", region: "Global" },
-  { name: "Main Website", url: "https://www.hostra.fun", uptime: "99.9%", region: "Global" },
-  { name: "API Service", url: "https://api.haneknodes.dpdns.org", uptime: "99.8%", region: "India" },
+  { name: "Game Panel", url: "https://panel.legacycloud.fun", uptime: "99.9%", region: "Global" },
+  { name: "Free Panel", url: "https://gp.legacycloud.fun", uptime: "99.9%", region: "Global" },
+  { name: "Vps ", url: "https://hvm.legacycloud.fun", uptime: "99.8%", region: "India" },
   { name: "Database Cluster", url: "db-primary", uptime: "100%", region: "Internal" },
 ];
